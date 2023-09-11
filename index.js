e34c0ee7dbebb9dee487157201fc481601b20e7b
Elaine Lai <elaineelaii@hotmail.com>
2023-09-11 11:55:40 -0700

    rerouted css

--- index.js
+++ index.js
@@ -18,9 +18,240 @@ app.get('/', (req, res) => {
 app.use(express.static('public'));
 
 
-// app.get('/api/users', (req, res) => {
-//   res.json(allParksFile)
-// })
+app.get('/api/users', (req, res) => {
+  const campsites = [
+    {
+      area: 'Coastal Mainland',
+      campground: 'Alice Lake',
+      subcampground: ' Walk-In ',
+      amenities: [
+        ' Swimming ',     ' Parking ',
+        ' Hiking ',       ' Boat Launch ',
+        ' Day Use Area ', ' Beach ',
+        ' Dog Beach ',    ' Information ',
+        ' Dump Station ', ' Restroom ',
+        ' No Dogs ',      ' No Fishing ',
+        ' Water Tap '
+      ]
+    },
+    {
+      area: 'Coastal Mainland',
+      campground: 'Alice Lake',
+      subcampground: 'B (Sites 56-96)',
+      amenities: [
+      ' Parking ',
+      ' Hiking ',
+      ' Entrance Kiosk ',
+      ' Information ',
+      ' Restroom ',
+      ' Restroom with Showers ',
+      ' Swimming ',
+      ' Playground ',
+      ' Camping ',
+      ' Walk-in Camping ',
+      ' No Dogs ',
+      ' No Cars '
+      ]
+    },
+    {
+      area: 'Coastal Mainland',
+      campground: ' Birkenhead Lake ',
+      subcampground: 'null',
+      amenities: [
+        ' Swimming ',     ' Parking ',
+        ' Hiking ',       ' Boat Launch ',
+        ' Day Use Area ', ' Beach ',
+        ' Dog Beach ',    ' Information ',
+        ' Dump Station ', ' Restroom ',
+        ' No Dogs ',      ' No Fishing ',
+        ' Water Tap '
+      ]
+    },
+    {
+      area: 'Coastal Mainland',
+      campground: ' S⨱ótsaqel / Chilliwack Lake ',
+      subcampground: ' Flora Loop ',
+      amenities: [
+        ' Hiking ',
+        ' Mountain Biking ',
+        ' Horseback Riding ',
+        ' Swimming ',
+        ' Day Use Area ',
+        ' Boat Launch ',
+        ' Group Camping ',
+        ' Camping ',
+        ' Parking ',
+        ' Group Camping '
+      ]
+    },
+    {
+      area: 'Coastal Mainland',
+      campground: ' S⨱ótsaqel / Chilliwack Lake ',
+      subcampground: 'Paleface Loop',
+      amenities: [ ' Restroom ', ' Fishing ', ' Swimming ', ' Water Tap ' ]
+    },
+    {
+      area: 'Coastal Mainland',
+      campground: ' Cultus Lake ',
+      subcampground: ' Maple Bay ',
+      amenities: [
+        ' Group Camping ',
+        ' Camping ',
+        ' Hiking ',
+        ' Parking ',
+        ' Swimming ',
+        ' Restroom ',
+        ' Lookout ',
+        ' Information ',
+        ' Day Use Area ',
+        ' Entrance Kiosk ',
+        ' Park Office ',
+        ' Boat Launch ',
+        ' Dump Station ',
+        ' Biking ',
+        ' Horseback Riding ',
+        ' Fish Cleaning Station '
+      ]
+    },
+    {
+      area: 'Coastal Mainland',
+      campground: ' Golden Ears ',
+      subcampground: ' North Beach ',
+      amenities: [
+        ' Group Camping ',
+        ' Camping ',
+        ' Hiking ',
+        ' Parking ',
+        ' Swimming ',
+        ' Restroom ',
+        ' Lookout ',
+        ' Information ',
+        ' Day Use Area ',
+        ' Entrance Kiosk ',
+        ' Park Office ',
+        ' Boat Launch ',
+        ' Dump Station ',
+        ' Biking ',
+        ' Horseback Riding ',
+        ' Fish Cleaning Station '
+      ]
+    },
+    {
+      area: 'Coastal Mainland',
+      campground: ' Inland Lake ',
+      subcampground: 'Campground',
+      amenities: [
+        ' Parking ',
+        ' Restroom ',
+        ' Garbage ',
+        ' Park Office ',
+        ' Water Tap ',
+        ' Accessible '
+      ]
+    },
+    {
+      area: 'Coastal Mainland',
+      campground: ' Nairn Falls ',
+      subcampground: 'null',
+      amenities: [
+        ' Parking ',
+        ' Hiking ',
+        ' Information ',
+        ' Day Use Area ',
+        ' Lookout ',
+        ' Restroom ',
+        ' Water Tap ',
+        ' Accessible '
+      ]
+    },
+    {
+      area: 'Coastal Mainland',
+      campground: ' Porpoise Bay ',
+      subcampground: 'Campground',
+      amenities: [
+        ' Parking ',
+        ' Campfire Circle ',
+        ' Recycling ',
+        ' Garbage ',
+        ' Restroom ',
+        ' Restroom with Showers ',
+        ' Information ',
+        ' Play Field ',
+        ' Water Tap ',
+        ' Accessible '
+      ]
+    },
+    {
+      area: 'Coastal Mainland',
+      campground: ' Rolley Lake ',
+      subcampground: 'Campground',
+      amenities: [
+        ' Waterfall ',
+        ' Hiking ',
+        ' Fishing ',
+        ' Information ',
+        ' Restroom ',
+        ' Restroom with Showers ',
+        ' Playground ',
+        ' Water Tap ',
+        ' No Swimming '
+      ]
+    },
+    {
+      area: 'Coastal Mainland',
+      campground: ' Saltery Bay ',
+      subcampground: 'Campground',
+      amenities: [
+        ' Accessible ',
+        ' Information ',
+        ' Dump Station ',
+        ' Hiking ',
+        ' Restroom ',
+        ' Garbage ',
+        ' Change House ',
+        ' Parking ',
+        ' Day Use Area ',
+        ' Water Tap '
+      ]
+    },
+    {
+      area: 'Coastal Mainland',
+      campground: ' Silver Lake ',
+      subcampground: 'null',
+      amenities: [
+        ' Information ',
+        ' Garbage ',
+        ' Recycling ',
+        ' Firewood ',
+        ' Restroom ',
+        ' Parking ',
+        ' Day Use Area ',
+        ' Boat Launch ',
+        ' Fishing ',
+        ' Water Tap ',
+        ' Accessible '
+      ]
+    },
+    {
+      area: 'Coastal Mainland',
+      campground: ' Sasquatch ',
+      subcampground: ' Lakeside Campground ',
+      amenities: [
+        ' Entrance Kiosk ',
+        ' Parking ',
+        ' Restroom ',
+        ' Playground ',
+        ' Dump Station ',
+        ' Day Use Area ',
+        ' Beach ',
+        ' Boat Launch ',
+        ' Fishing ',
+        ' Swimming '
+      ]
+    }
+  ];
+  res.json(campsites)
+})
 
 app.listen(5000, () => {
   console.log('Server is listening on port 5000')
