d34989d55122d40145ae7c63e73ff05fdd24c4ab
Elaine Lai <elaineelaii@hotmail.com>
2023-09-11 11:08:32 -0700

    defined dirname to index.js

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 import express from 'express';
 import path from 'path';
+import { fileURLToPath } from 'url';
 
 const app = express()
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+console.log(__dirname)
 
 app.get('/', (req, res) => {
   // res.send('express vercel app response!')
