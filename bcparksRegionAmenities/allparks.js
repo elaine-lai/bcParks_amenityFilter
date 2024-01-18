588dd9a905b4f23a8127cc64336790099772c8a7
Elaine Lai <elaineelaii@hotmail.com>
2024-01-18 11:04:06 -0800

    edited for it to take the json file from an import instead of hard coding it into index.js

---
+++ bcparksRegionAmenities/allparks.js
@@ -0,0 +1,748 @@
+const jsonData = [
+  {
+    "area": "Coastal Mainland",
+    "campground": "Alice Lake",
+    "subcampground": " Walk-In ",
+    "amenities": [
+      " Swimming ",     " Parking ",
+      " Hiking ",       " Boat Launch ",
+      " Day Use Area ", " Beach ",
+      " Dog Beach ",    " Information ",
+      " Dump Station ", " Restroom ",
+      " No Dogs ",      " No Fishing ",
+      " Water Tap "
+    ]
+  },
+  {
+	  "area": "Coastal Mainland",
+	  "campground": "Alice Lake",
+	  "subcampground": "A (Sites 1-55)",
+	  "amenities": [
+		" Group Camping ",
+		" Entrance Kiosk ",
+		" Swimming ",
+    " Parking ",
+		" Change House ",
+		" Walk-in Camping ",
+		" Playground ",
+    " Hiking ",
+		" Information ",
+    " Dump Station ",
+    " Day Use Area ",
+		" Restroom ",
+		" Restroom with Showers ",
+		" Amphitheatre ",
+		" No Dogs "
+	  ]
+  },
+  {
+	  "area": "Coastal Mainland",
+	  "campground": "Alice Lake",
+	  "subcampground": "B (Sites 56-96)",
+	  "amenities": [
+		" Parking ",
+		" Hiking ",
+		" Entrance Kiosk ",
+		" Information ",
+		" Restroom ",
+		" Restroom with Showers ",
+		" Swimming ",
+		" Playground ",
+		" Camping ",
+		" Walk-in Camping ",
+		" No Dogs ",
+		" No Cars "
+	  ]
+  },
+  {
+    "area": "Coastal Mainland",
+    "campground": " Birkenhead Lake ",
+    "subcampground": "null",
+    "amenities": [
+      " Swimming ",     " Parking ",
+      " Hiking ",       " Boat Launch ",
+      " Day Use Area ", " Beach ",
+      " Dog Beach ",    " Information ",
+      " Dump Station ", " Restroom ",
+      " No Dogs ",      " No Fishing ",
+      " Water Tap "
+    ]
+  },
+  {
+    "area": "Coastal Mainland",
+    "campground": " S⨱ótsaqel / Chilliwack Lake ",
+    "subcampground": " Flora Loop ",
+    "amenities": [
+      " Restroom "
+    ]
+  },
+  {
+    "area": "Coastal Mainland",
+    "campground": " S⨱ótsaqel / Chilliwack Lake ",
+    "subcampground": " Lindeman Loop ",
+    "amenities": [
+      " Restroom ",
+      " Water Tap "
+    ]
+  },
+  {
+    "area": "Coastal Mainland",
+    "campground": " S⨱ótsaqel / Chilliwack Lake ",
+    "subcampground": " Greendrop Loop ",
+    "amenities": [
+      " Restroom ",
+      " Water Tap "
+    ]
+  },
+  {
+    "area": "Coastal Mainland",
+    "campground": " S⨱ótsaqel / Chilliwack Lake ",
+    "subcampground": "Paleface Loop",
+    "amenities": [ " Restroom ", " Fishing ", " Swimming ", " Water Tap " ]
+  },
+  {
+    "area": "Coastal Mainland",
+    "campground": " Cultus Lake ",
+    "subcampground": " Delta Grove ",
+    "amenities": [
+      " Information ",
+      " Water Tap ",
+      " Restroom with Showers ",
+      " Restroom ",
+      " Accessible"
+    ]
+  },
+  {
+    "area": "Coastal Mainland",
+    "campground": " Cultus Lake ",
+    "subcampground": " Maple Bay ",
+    "amenities": [
+      " Information ",
+      " Water Tap ",
+      " Restroom with Showers "
+    ]
+  },
+  {
+    "area": "Coastal Mainland",
+    "campground": " Golden Ears ",
+    "subcampground": " North Beach ",
+    "amenities": [
+      " Beach ",
+      " Restroom ",
+      " Hiking ",
+      " Pets Permitted on Leash ",
+      " Information ",
+      " Water Tap "
+    ]
+  },
+  {
+    "area": "Coastal Mainland",
+    "campground": " Golden Ears ",
+    "subcampground": " Gold Creek ",
+    "amenities": [
+      " Information ",
+      " Restroom ",
+      " Restroom with Showers ",
+      " Parking ",
+      " Water Tap ",
+      " Food Locker "
+    ]
+  },
+  {
+    "area": "Coastal Mainland",
+    "campground": " Golden Ears ",
+    "subcampground": " Alouette North ",
+    "amenities": [
+      " Restroom ",
+      " Playground ",
+      " Restroom with Showers ",
+      " Water Tap "
+    ]
+  },
+  {
+    "area": "Coastal Mainland",
+    "campground": " Golden Ears ",
+    "subcampground": " Alouette South ",
+    "amenities": [
+      " Restroom ",
+      " Restroom with Showers ",
+      " Water Tap "
+    ]
+  },
+  {
+    "area": "Coastal Mainland",
+    "campground": " Inland Lake ",
+    "subcampground": "Campground",
+    "amenities": [
+      " Parking ",
+      " Restroom ",
+      " Garbage ",
+      " Park Office ",
+      " Water Tap ",
+      " Accessible "
+    ]
+  },
+  {
+    "area": "Coastal Mainland",
+    "campground": " Nairn Falls ",
+    "subcampground": "null",
+    "amenities": [
+      " Parking ",
+      " Hiking ",
+      " Information ",
+      " Day Use Area ",
+      " Lookout ",
+      " Restroom ",
+      " Water Tap ",
+      " Accessible "
+    ]
+  },
+  {
+    "area": "Coastal Mainland",
+    "campground": " Porpoise Bay ",
+    "subcampground": "Campground",
+    "amenities": [
+      " Parking ",
+      " Campfire Circle ",
+      " Recycling ",
+      " Garbage ",
+      " Restroom ",
+      " Restroom with Showers ",
+      " Information ",
+      " Play Field ",
+      " Water Tap ",
+      " Accessible "
+    ]
+  },
+  {
+    "area": "Coastal Mainland",
+    "campground": " Rolley Lake ",
+    "subcampground": "Campground",
+    "amenities": [
+      " Waterfall ",
+      " Hiking ",
+      " Fishing ",
+      " Information ",
+      " Restroom ",
+      " Restroom with Showers ",
+      " Playground ",
+      " Water Tap ",
+      " No Swimming "
+    ]
+  },
+  {
+    "area": "Coastal Mainland",
+    "campground": " Saltery Bay ",
+    "subcampground": "Campground",
+    "amenities": [
+      " Accessible ",
+      " Information ",
+      " Dump Station ",
+      " Hiking ",
+      " Restroom ",
+      " Garbage ",
+      " Change House ",
+      " Parking ",
+      " Day Use Area ",
+      " Water Tap "
+    ]
+  },
+  {
+    "area": "Coastal Mainland",
+    "campground": " Silver Lake ",
+    "subcampground": "null",
+    "amenities": [
+      " Information ",
+      " Garbage ",
+      " Recycling ",
+      " Firewood ",
+      " Restroom ",
+      " Parking ",
+      " Day Use Area ",
+      " Boat Launch ",
+      " Fishing ",
+      " Water Tap ",
+      " Accessible "
+    ]
+  },
+  {
+    "area": "Coastal Mainland",
+    "campground": " Sasquatch ",
+    "subcampground": " Lakeside Campground ",
+    "amenities": [
+      " Entrance Kiosk ",
+      " Parking ",
+      " Restroom ",
+      " Playground ",
+      " Dump Station ",
+      " Day Use Area ",
+      " Beach ",
+      " Boat Launch ",
+      " Fishing ",
+      " Swimming "
+    ]
+  },
+  {
+    "area": "Coastal Mainland",
+    "campground": " Sasquatch ",
+    "subcampground": " Hicks Campground ",
+    "amenities": [
+      " Swimming ",
+      " Day Use Area ",
+      " Amphitheatre ",
+      " Restroom ",
+      " Water Tap "
+    ]
+  },
+  {
+    "area": "Coastal Mainland",
+    "campground": " Silver Lake ",
+    "subcampground": " null ",
+    "amenities": [
+      " Information ",
+      " Garbage ",
+      " Recycling ",
+      " Firewood ",
+      " Restroom ",
+      " Parking ",
+      " Day Use Area ",
+      " Boat Launch ",
+      " Fishing ",
+      " Water Tap ",
+      " Accessible "
+    ]
+  },
+  {
+    "area": "Southern Interior",
+    "campground": "Blanket Creek",
+    "subcampground": "Campground",
+    "amenities": [
+      " Hiking ",
+      " Biking ",
+      " Information ",
+      " Park Office ",
+      " Restroom ",
+      " Restroom with Showers ",
+      " Firewood ",
+      " Recycling ",
+      " Grey Water Disposal ",
+      " Water Tap ",
+      " Food Locker "
+    ]
+  },
+  {
+    "area": "Southern Interior",
+    "campground": " Bromley Rock ",
+    "subcampground": "null",
+    "amenities": [
+      " Day Use Area ",
+      " Fishing ",
+      " Restroom ",
+      " Information ",
+      " Garbage ",
+      " Water Tap ",
+      " Parking ",
+      " Beach "
+    ]
+  },
+  {
+    "area": "Southern Interior",
+    "campground": " Champion Lakes ",
+    "subcampground": "Campground",
+    "amenities": [
+      " Hiking ",
+      " Information ",
+      " Amphitheatre ",
+      " Restroom ",
+      " Water Tap "
+    ]
+  },
+  {
+    "area": "Southern Interior",
+    "campground": " Dry Gulch ",
+    "subcampground": "Campground",
+    "amenities": [ " Information ", " Entrance Kiosk ", " Restroom " ]
+  },
+  {
+    "area": "Southern Interior",
+    "campground": " E.C. Manning ",
+    "subcampground": "Lightning Lake",
+    "amenities": [
+      " Parking ",
+      " Hiking ",
+      " Day Use Area ",
+      " Amphitheatre ",
+      " Camping ",
+      " Entrance Kiosk ",
+      " Information ",
+      " Firewood ",
+      " Water Tap "
+    ]
+  },
+  {
+    "area": "Southern Interior",
+    "campground": " Ellison ",
+    "subcampground": "Campground",
+    "amenities": [
+      " Hiking ",
+      " Self Guided Nature Trail ",
+      " Swimming ",
+      " Amphitheatre ",
+      " Playground ",
+      " Day Use Area ",
+      " Information ",
+      " Pay Phone ",
+      " Restroom ",
+      " Restroom with Showers ",
+      " Water Tap "
+    ]
+  },
+  {
+    "area": "Southern Interior",
+    "campground": " Gladstone ",
+    "subcampground": "null",
+    "amenities": [
+      " Parking ",
+      " Information ",
+      " Hiking ",
+      " Boat Launch ",
+      " Restroom ",
+      " Restroom with Showers ",
+      " Beach ",
+      " Swimming ",
+      " Fishing ",
+      " Canoeing ",
+      " Water Tap ",
+      " Playground "
+    ]
+  },
+  {
+    "area": "Southern Interior",
+    "campground": " sw̓iw̓s (Haynes Point) ",
+    "subcampground": "null",
+    "amenities": [
+      " Camping ",
+      " Restroom ",
+      " Information ",
+      " Day Use Area ",
+      " Parking ",
+      " Change House ",
+      " Boat Launch ",
+      " Canoeing ",
+      " Fishing ",
+      " Swimming ",
+      " Water Tap "
+    ]
+  },
+  {
+    "area": "Southern Interior",
+    "campground": " Herald ",
+    "subcampground": "Campgrounds",
+    "amenities": [
+      " Parking ",
+      " Restroom ",
+      " Restroom with Showers ",
+      " Dump Station "
+    ]
+  },
+  {
+    "area": "Southern Interior",
+    "campground": " Juniper Beach ",
+    "subcampground": "Sites 1-16, 22-32",
+    "amenities": [
+      " Parking ",
+      " Boat Launch ",
+      " Restroom ",
+      " Restroom with Showers ",
+      " Dump Station ",
+      " Information "
+    ]
+  },
+  {
+    "area": "Islands",
+    "campground": "Bamberton",
+    "subcampground": "Sites 1-48",
+    "amenities": [ " Information ", " Restroom ", " Hiking ", " Water Tap " ]
+  },
+  {
+    "area": "Islands",
+    "campground": " Elk Falls ",
+    "subcampground": "Campground",
+    "amenities": [
+      " Parking ",
+      " Day Use Area ",
+      " Information ",
+      " Playground ",
+      " Restroom ",
+      " Dump Station ",
+      " Water Tap "
+    ]
+  },
+  {
+    "area": "Islands",
+    "campground": " Fillongley ",
+    "subcampground": "Campground",
+    "amenities": [
+      " Hiking ",
+      " Information ",
+      " Parking ",
+      " Garbage ",
+      " Day Use Area ",
+      " Accessible ",
+      " Restroom ",
+      " Water Tap "
+    ]
+  },
+  {
+    "area": "Islands",
+    "campground": " Loveland Bay ",
+    "subcampground": "null",
+    "amenities": [
+      " Parking ",
+      " Information ",
+      " Boat Launch ",
+      " Beach ",
+      " Day Use Area ",
+      " Restroom ",
+      " Grey Water ",
+      " Water Tap ",
+      " Accessible "
+    ]
+  },
+  {
+    "area": "Islands",
+    "campground": " Miracle Beach ",
+    "subcampground": " Sites 126-201, G1 ",
+    "amenities": [ " Parking ", " Hiking ", " Camping ", " Restroom " ]
+  },
+  {
+    "area": "Islands",
+    "campground": " Morton Lake ",
+    "subcampground": "null",
+    "amenities": [
+      " Parking ",
+      " Boat Launch ",
+      " Information ",
+      " Hiking ",
+      " Beach ",
+      " Fishing ",
+      " Swimming ",
+      " Day Use Area ",
+      " Restroom ",
+      " Water Tap "
+    ]
+  },
+  {
+    "area": "Islands",
+    "campground": " Saysutshun (Newcastle Island Marine) ",
+    "subcampground": "Campground",
+    "amenities": [
+      " Park Office ",
+      " Swimming ",
+      " Beach ",
+      " Marina ",
+      " Day Use Area ",
+      " Information ",
+      " Restroom ",
+      " Restroom with Showers ",
+      " Firewood ",
+      " Concession ",
+      " Picnic Shelter ",
+      " Water Tap "
+    ]
+  },
+  {
+    "area": "Islands",
+    "campground": " Rathtrevor Beach ",
+    "subcampground": " Sites 175 - 226 ",
+    "amenities": [ " Hiking ", " Camping ", " Accessible " ]
+  },
+  {
+    "area": "Islands",
+    "campground": " Smelt Bay ",
+    "subcampground": "null",
+    "amenities": [
+      " Parking ",
+      " Beach ",
+      " Hiking ",
+      " Information ",
+      " Restroom ",
+      " Picnic Shelter ",
+      " Camping ",
+      " Water Tap "
+    ]
+  },
+  {
+    "area": "Northern",
+    "campground": "Tā Ch’ilā (Boya Lake)",
+    "subcampground": "Campground",
+    "amenities": [
+      " Dump Station ",
+      " Parking ",
+      " Garbage ",
+      " Restroom ",
+      " Information ",
+      " Playground ",
+      " Day Use Area ",
+      " Picnic Shelter ",
+      " Boat Launch ",
+      " Accessible ",
+      " Water Tap "
+    ]
+  },
+  {
+    "area": "Northern",
+    "campground": " Carp Lake ",
+    "subcampground": " SS2 ",
+    "amenities": [
+      " Fishing ",      " Canoeing ",
+      " Swimming ",     " Beach ",
+      " Boat Launch ",  " Lookout ",
+      " Lookout ",      " Camping ",
+      " Parking ",      " Restroom ",
+      " Day Use Area ", " Water Tap "
+    ]
+  },
+  {
+    "area": "Northern",
+    "campground": " Crooked River ",
+    "subcampground": "null",
+    "amenities": [
+      " Information ",
+      " Park Office ",
+      " Parking ",
+      " Restroom ",
+      " Playground ",
+      " Hiking ",
+      " Picnic Shelter ",
+      " Beach ",
+      " Day Use Area ",
+      " Camping ",
+      " Change House ",
+      " Canoeing ",
+      " Dump Station ",
+      " Restroom with Showers ",
+      " Accessible ",
+      " Water Tap "
+    ]
+  },
+  {
+    "area": "Northern",
+    "campground": " Kleanza Creek ",
+    "subcampground": "null",
+    "amenities": [
+      " Parking ",
+      " Horseshoes ",
+      " Information ",
+      " Garbage ",
+      " Day Use Area ",
+      " Restroom ",
+      " Water Tap "
+    ]
+  },
+  {
+    "area": "Northern",
+    "campground": " Liard River Hot Springs ",
+    "subcampground": "null",
+    "amenities": [
+      " Camping ",      " Lookout ",
+      " Parking ",      " Change House ",
+      " Restroom ",     " Information ",
+      " Playground ",   " Marsh ",
+      " Day Use Area ", " Picnic Shelter ",
+      " Garbage ",      " Accessible ",
+      " No Dogs ",      " No Bicycling ",
+      " Water Tap ",    " Food Locker ",
+      " Gate "
+    ]
+  },
+  {
+    "area": "Northern",
+    "campground": " Meziadin Lake ",
+    "subcampground": "Sites 25-66",
+    "amenities": [
+      " Boat Launch ",
+      " Restroom ",
+      " Picnic Shelter ",
+      " Garbage ",
+      " Information ",
+      " Store ",
+      " Water Tap ",
+      " Accessible "
+    ]
+  },
+  {
+    "area": "Northern",
+    "campground": " Mount Robson ",
+    "subcampground": " Lucerne Campground ",
+    "amenities": [
+      " Information ",
+      " Park Office ",
+      " Parking ",
+      " Restroom ",
+      " Playground ",
+      " Hiking ",
+      " Picnic Shelter ",
+      " Beach ",
+      " Day Use Area ",
+      " Camping ",
+      " Change House ",
+      " Canoeing ",
+      " Dump Station ",
+      " Restroom with Showers ",
+      " Accessible ",
+      " Water Tap "
+    ]
+  },
+  {
+    "area": "Northern",
+    "campground": " Paarens Beach ",
+    "subcampground": "null",
+    "amenities": [
+      " Fishing ",
+      " Boat Launch ",
+      " Swimming ",
+      " Parking ",
+      " Information ",
+      " Firewood ",
+      " Day Use Area ",
+      " Picnic Shelter ",
+      " Restroom ",
+      " Change House ",
+      " Playground ",
+      " Water Tap ",
+      " Accessible "
+    ]
+  },
+  {
+    "area": "Northern",
+    "campground": " Purden Lake ",
+    "subcampground": "Campground",
+    "amenities": [
+      " Parking ",
+      " Boat Launch ",
+      " Dump Station ",
+      " Restroom ",
+      " Firewood ",
+      " Playground ",
+      " Information ",
+      " Water Tap "
+    ]
+  },
+  {
+    "area": "Northern",
+    "campground": " Red Bluff ",
+    "subcampground": "null",
+    "amenities": [
+      " Fishing ",      " Canoeing ",
+      " Swimming ",     " Beach ",
+      " Boat Launch ",  " Lookout ",
+      " Lookout ",      " Camping ",
+      " Parking ",      " Restroom ",
+      " Day Use Area ", " Water Tap "
+    ]
+  }
+
+]
+
+export default jsonData;
